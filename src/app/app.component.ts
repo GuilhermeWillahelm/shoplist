62f308b29cd62af97444e8b12b64b4f78e03cdd4
GuilhermeWillahelm <guilhermedevlm@outlook.com>
2024-08-13 12:38:20 -0300

    Add list-products and user-area.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { ListProductsComponent } from './list-products/list-products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent, ListProductsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'shoplist';
+  title = 'ShopList';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './user-area/login/login.component';
+import { RegisterComponent } from './user-area/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'appLogin', component: LoginComponent},
+    {path: 'appRegister', component: RegisterComponent}
+];
